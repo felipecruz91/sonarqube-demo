6c2a3b4c697458b8afa3b72dcc477a7f48833587
Felipe Cruz Martinez <15997951+felipecruz91@users.noreply.github.com>
2023-10-26 17:52:29 +0200

    Update main.go

--- main.go
+++ main.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/labstack/echo"
 )
 
 func main() {
+	// just produce duplicated code to make the SonarQube quality gate fail
+	fmt.Println("hi")
+	fmt.Println("hi")
+	fmt.Println("hi")
+	fmt.Println("hi")
+	
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
